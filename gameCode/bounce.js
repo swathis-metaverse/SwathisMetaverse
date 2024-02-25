3753b76c71fba586ddf31e9caabb5186b191c851
alecdog04 <75770101+alecdog04@users.noreply.github.com>
2024-02-25 15:21:11 -0800

    Add files via upload

--- gameCode/bounce.js
+++ gameCode/bounce.js
@@ -250,9 +250,16 @@ function collisionCircle(ball){
         ball.color = "rgb(" + r + "," + g + "," + b + ")";
         
         // Check if the play note checkbox is checked and play the note
-        if (document.getElementById('playNoteCheckbox').checked) {
+        var audioFileInput = document.getElementById('audioFileInput');
+        if (document.getElementById('playNoteCheckbox').checked && audioFileInput.files.length === 0) {
             playNote(ball.noteIndex);
             ball.noteIndex = (ball.noteIndex + 1) % musicalScale.length;
+        } else if (document.getElementById('playNoteCheckbox').checked) {
+            if (audioFileInput.files.length != 0) { 
+                var audioFile = audioFileInput.files[0];
+                var audio = new Audio(URL.createObjectURL(audioFile));
+                audio.play();
+            }
         }
     }
 }
